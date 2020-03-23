167348c20a1986c1923545ecccc65e7e4e668570
Nileshkumar <nileshmaurya250@gmail.com>
2020-03-23 17:59:44 +0530

    added input field

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
     </div>
     <router-view />
   </div>
 </template>
 
 <style>
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  /* outline: 1px solid red; */
+}
+
+body {
+  max-width: 400px;
+  margin: 0 auto;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
 #nav {
   padding: 30px;
+  text-align: center;
 }
 
 #nav a {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,82 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="calculator">
+      <div class="calculator__input">
+        <div
+          class="input"
+          :value="inputText"
+          @input="calculate"
+          inputMode="numeric"
+          contenteditable="true"
+        >
+          0
+        </div>
+      </div>
+      <div class="calculator__result">{{ result }}</div>
+      <span class="hr"></span>
+      <div class="calculator__btns"></div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      inputText: "0",
+      result: ""
+    };
+  },
+  methods: {
+    calculate(event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+      }
+      this.inputText = event.target.textContent;
+      this.result = this.inputText;
+    }
+  },
+  components: {}
 };
 </script>
+
+<style scoped>
+.calculator {
+  background-color: #fefefe;
+  padding: 1rem;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+}
+
+.calculator__result {
+  text-align: right;
+  overflow-x: scroll;
+  scrollbar-width: none;
+}
+
+.hr {
+  display: block;
+  width: 100%;
+  height: 2px;
+  margin: 0 auto;
+  border-bottom: 1px solid rgba(90, 90, 90, 0.05);
+}
+
+.input {
+  font-size: 2rem;
+  text-align: right;
+  margin-left: auto;
+  line-height: 2;
+  color: #019b55;
+  overflow-x: scroll;
+  scrollbar-width: none;
+}
+
+.input:focus {
+  outline: 0;
+  border: 0;
+}
+</style>
