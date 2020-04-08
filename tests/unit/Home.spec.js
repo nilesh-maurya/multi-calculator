3620a8ccd7ff518456dbb313d1d78c213a5cd0be
Nileshkumar <nileshmaurya250@gmail.com>
2020-04-08 15:19:56 +0530

    Added basic tests for keypadButton components

---
+++ tests/unit/Home.spec.js
@@ -0,0 +1,74 @@
+import { mount, shallowMount, createLocalVue } from "@vue/test-utils";
+import VueDOMPurifyHTML from "vue-dompurify-html";
+import Home from "@/views/Home.vue";
+
+const localVue = createLocalVue();
+localVue.use(VueDOMPurifyHTML);
+
+describe("testing Home.vue", () => {
+  describe("basic Keypad", () => {
+    it("renders basic keypad", () => {
+      const wrapper = shallowMount(Home, { localVue });
+
+      expect(wrapper.find(".input").text()).toBe("0");
+      expect(wrapper.find(".calculator__result").text()).toBe("");
+    });
+
+    it("testing numbers key", async () => {
+      const wrapper = mount(Home, { localVue });
+
+      // input and result container
+      const inputDiv = wrapper.find(".input");
+      const resultDiv = wrapper.find(".calculator__result");
+      const buttonWrapper = wrapper.findAll("button[data-type='number']");
+
+      for (let i = 0; i < buttonWrapper.length; i++) {
+        const button = buttonWrapper.at(i);
+
+        wrapper.setData({ inputText: ["0"], result: "" });
+        button.trigger("click");
+
+        await wrapper.vm.$nextTick();
+
+        const emittedValue = button.emitted()["click-keypad-btn"][0][0].value;
+
+        // console.log(inputDiv.text(), resultDiv.text(), emittedValue);
+
+        if (emittedValue !== ".") {
+          expect(inputDiv.text()).toBe(emittedValue);
+          expect(resultDiv.text()).toBe("= " + emittedValue);
+        } else {
+          expect(inputDiv.text()).toBe("0" + emittedValue);
+          expect(resultDiv.text()).toBe("= 0");
+        }
+      }
+    });
+
+    // it("testing operator key", async () => {
+    //   const wrapper = mount(Home, { localVue });
+    //   const inputDiv = wrapper.find(".input");
+    //   const resultDiv = wrapper.find(".calculator__result");
+
+    //   const operatorBtnWrapper = wrapper.findAll(
+    //     "button[data-type='operator']"
+    //   );
+
+    //   for (let i = 0; i < operatorBtnWrapper.length; i++) {
+    //     const operatorBtn = operatorBtnWrapper.at(i);
+    //     operatorBtn.trigger("click");
+
+    //     await wrapper.vm.$nextTick();
+
+    //     const emittedValue = operatorBtn.emitted()["click-keypad-btn"][0][0]
+    //       .value;
+
+    //     console.log(
+    //       inputDiv.text(),
+    //       resultDiv.text(),
+    //       emittedValue,
+    //       inputDiv.text() == "0" + emittedValue
+    //     );
+    //   }
+    // });
+  });
+});
