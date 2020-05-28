0fd9dd2a662def1c574c5b82aabc42f2e7f85470
Nileshkumar <nileshmaurya250@gmail.com>
2020-05-28 11:14:45 +0530

    completed Speed and Time Component

---
+++ src/components/convert/Time.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="time wrapper-convert-element">
+    <v-row align="center" class="wrapper__item">
+      <v-col cols="4">
+        <v-select
+          v-model="select1"
+          :items="items"
+          item-text="unit"
+          item-value="abbr"
+          item-color="green"
+          :hint="select1.abbr"
+          persistent-hint
+          return-object
+          color="#09b464"
+          @change="handleChange('1')"
+        ></v-select>
+      </v-col>
+      <span
+        :class="{ focus: toggleFocus }"
+        class="g-1 time__input-1"
+        data-id="1"
+        @click="toggleFocus = true"
+      >
+        {{ first_input }}
+      </span>
+    </v-row>
+    <v-row align="center" class="wrapper__item">
+      <v-col cols="4">
+        <v-select
+          v-model="select2"
+          :items="items"
+          item-text="unit"
+          item-value="abbr"
+          item-color="green"
+          :hint="select2.abbr"
+          persistent-hint
+          return-object
+          color="#09b464"
+          @change="handleChange('2')"
+        ></v-select>
+      </v-col>
+      <span
+        :class="{ focus: !toggleFocus }"
+        class="g-2 time__input-2"
+        data-id="2"
+        @click="toggleFocus = false"
+      >
+        {{ second_input }}
+      </span>
+    </v-row>
+    <numeric-keypad @numeric-key-event="handleInput"></numeric-keypad>
+  </div>
+</template>
+
+<script>
+import NumericKeypad from "../NumericKeypad";
+import { getters, mutations, actions } from "../../utils/numeric-keypad-store";
+import { convert } from "../../utils/conversion";
+
+export default {
+  name: "Time",
+  created() {
+    actions.reset();
+  },
+  data() {
+    return {
+      toggleFocus: true,
+      select1: { unit: "Decade", abbr: "decade" },
+      select2: { unit: "year", abbr: "year" },
+      items: [
+        { unit: "Century", abbr: "century" },
+        { unit: "Decade", abbr: "decade" },
+        { unit: "Year", abbr: "year" },
+        { unit: "Month", abbr: "month" },
+        { unit: "Week", abbr: "week" },
+        { unit: "Day", abbr: "day" },
+        { unit: "Hour", abbr: "hour" },
+        { unit: "Minute", abbr: "minute" },
+        { unit: "Second", abbr: "s" },
+        { unit: "Millisecond", abbr: "ms" },
+        { unit: "Microsecond", abbr: "us" },
+        { unit: "Picosecond", abbr: "ps" }
+      ]
+    };
+  },
+  computed: {
+    first_input() {
+      return getters.getFirstInput();
+    },
+    second_input() {
+      return getters.getSecondInput();
+    }
+  },
+  methods: {
+    handleInput(key) {
+      const id = document.querySelector("span.focus").dataset.id;
+
+      switch (key) {
+        case "AC": {
+          actions.reset(id);
+          break;
+        }
+        case "Backspace": {
+          actions.backspace(id);
+          break;
+        }
+        case ".": {
+          actions.decimal(id);
+          break;
+        }
+        default: {
+          actions.number(id, key, 10);
+        }
+      }
+      this.calculateTime(id);
+    },
+    handleChange(id) {
+      if (this.toggleFocus === true) {
+        id = "1";
+      } else {
+        id = "2";
+      }
+
+      this.calculateTime(id);
+    },
+    calculateTime(id) {
+      // find conversion here
+      let convertedValue;
+      if (id === "1") {
+        convertedValue = convert(this.first_input, "time")
+          .from(this.select1.abbr)
+          .to(this.select2.abbr);
+
+        mutations.setSecondInput(convertedValue.toString(10));
+      } else if (id === "2") {
+        convertedValue = convert(this.second_input, "time")
+          .from(this.select2.abbr)
+          .to(this.select1.abbr);
+        mutations.setFirstInput(convertedValue.toString(10));
+      }
+    }
+  },
+  components: {
+    NumericKeypad
+  }
+};
+</script>
