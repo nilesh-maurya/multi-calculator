219018a67a5d5a77ca7dc4e8bae8b84bffaa5efe
Nileshkumar <nileshmaurya250@gmail.com>
2020-06-07 21:10:48 +0530

    removed vue-dompurify-html as dependency

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
-import VueDompurifyHTML from "vue-dompurify-html";
 
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
-
-Vue.use(VueDompurifyHTML);
 
 new Vue({
   router,
